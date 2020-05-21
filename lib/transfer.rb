dc2feedf8d34ee58f338a13bfece5c2b86611e07
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-21 21:05:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,7 +12,7 @@ class Transfer
     sender.valid? && receiver.valid?
   end
 
-  def transfer 
+  def transfer
     if sender.balance > amount
       sender.balance -= amount
       receiver.deposit(amount)
