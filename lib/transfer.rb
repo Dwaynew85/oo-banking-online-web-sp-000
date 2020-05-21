6a369cad5ab7dc50ee7b8790da4c9d2dd497cdac
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-21 20:14:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class BankAccount
-  attr_accessor :name, :account, :balance 
+  attr_accessor :name, :account, :balance
 
   def initialize(name, account)
   @account = account
-  @name = name 
+  @name = name
   @balance = 1000
   end
 
